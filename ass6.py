47310a35c8927954cd52a41971760de037f9d4c1
muneebrw <57323681+muneebrw@users.noreply.github.com>
2019-12-21 19:23:43 +0500

    Add files via upload

---
+++ ass6.py
@@ -0,0 +1,40 @@
+class Car():
+    def __init__(self,name,color,model,company,weight):
+        self.name = name
+        self.color = color
+        self.model = model
+        self.company = company
+        self.weight = weight
+c1 = car('vitz', 'silver', 2012, 'toyota', '900kg')
+c2 = car('civic', 'red', 2018, 'honda', '950kg')
+c3 = car('city', 'black', 2015, 'honda', '800kg')
+c4 = car('bmw', 'black', 2017, 'bmw', '700kg')
+c5 = car('coure', 'green', 2013, 'dyhatsu', '660kg')
+    def color(car.color):
+        if self.color == color:
+            print('available')
+color(red)            
+
+    def model(car.model):
+        if car.model == model:
+            print('in stock')
+model(2012)
+
+    def name(car.name):
+        if car.name == name:
+            print('available')
+            
+            
+"""ans 1 - oop is a type of programming in which objects (classes) are used to do specific tasks.
+
+ans 2 - It provides a clear modular structure for programs which makes it good for defining abstract datatypes in which implementation details are hidden
+Objects can also be reused within an across applications. The reuse of software also lowers the cost of development. More effort is put into the object-oriented analysis and design, which lowers the overall cost of development.
+It makes software easier to maintain. Since the design is modular, part of the system can be updated in case of issues without a need to make large-scale changes
+
+
+ans 3 - we define a variable in ordinary programming to perform special tasks is called function while function in a class is called method.
+
+ans 4 - class - class is like a form in which information is gathered.
+object - a copy of a form is called an object.
+attribute - one information in a form (class) is counted as one attribute
+"""
